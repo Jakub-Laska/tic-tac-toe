5da311a2995b64b5cd61dd867668256c1d8ac876
Jakub-Laska <laska.rayl@gmail.com>
2025-06-29 14:31:25 +0200

    finish basic functionality

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let turn = 0;
 
+const winCombinations = [
+    [0, 1 ,2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6] 
+]
+
+
 const cells = document.querySelectorAll('.cell');
 
 // hover
 cells.forEach(cell => {
   cell.addEventListener('mouseenter', () => {
-    if (cell.classList.contains('black') || cell.classList.contains('purple')) {
+    if (cell.classList.contains('x-mark') || cell.classList.contains('o-mark')) {
       return;
     }
 
     if (turn === 0) {
-      cell.classList.remove('o');
-      cell.classList.add('x');
+      cell.classList.remove('o-hover');
+      cell.classList.add('x-hover');
     } else {
-      cell.classList.remove('x');
-      cell.classList.add('o');
+      cell.classList.remove('x-hover');
+      cell.classList.add('o-hover');
     }
   });
 });
@@ -23,24 +35,59 @@ cells.forEach(cell => {
 // click
 cells.forEach(cell => {
   cell.addEventListener('click', () => {
-        if (cell.classList.contains('black') || cell.classList.contains('purple')) {
+        if (cell.classList.contains('x-mark') || cell.classList.contains('o-mark')) {
       return;
     }
 
 
     if (turn === 0) {
-        cell.classList.remove('x');
-        cell.classList.add('black');
-        cell.classList.add('notAllowed')
+        cell.classList.remove('x-hover');
+        cell.classList.add('x-mark', 'notAllowed');
         turn = 1;
     } else {
-      cell.classList.remove('o');
-        cell.classList.add('purple');
-        cell.classList.add('notAllowed')
+      cell.classList.remove('o-hover');
+        cell.classList.add('o-mark', 'notAllowed');
         turn = 0;
     }
+    checkWin();
   });
 });
 
+function checkWin() {
+      for (let player of ['x', 'o']) {
+    for (let [a, b, c] of winCombinations) {
+      if (
+        cells[a].classList.contains(`${player}-mark`) &&
+        cells[b].classList.contains(`${player}-mark`) &&
+        cells[c].classList.contains(`${player}-mark`)
+      ) {
+        winMessage(player);
+        return player;
+      }
+    }
+  }
 
+  // Check for draw
+  const isDraw = [...cells].every(cell =>
+    cell.classList.contains('x-mark') || cell.classList.contains('o-mark')
+  );
 
+  if (isDraw) {
+    drawMessage();
+    return true;
+  }
+
+  return false;
+}
+
+function winMessage(player) {
+        const modal = document.getElementById('modal');
+        modal.classList.toggle('hidden');
+        alert(`player: ${player.toUpperCase()} wins!`);
+}
+
+function drawMessage() {
+        const modal = document.getElementById('modal');
+        modal.classList.toggle('hidden');
+        alert('it is a draw');
+}
